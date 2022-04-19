1d316588c026de67bc761f280e13f0f7cb9419d1
Winsey Li <winsey@storypark.com>
2022-04-19 21:29:07 +1200

    Initial Commit

---
+++ try-new-tech-ios-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  try-new-tech-ios-app
+//
+//  Created by Winsey Li on 19/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
